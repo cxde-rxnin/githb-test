3187eb664b09e6d8ea5d1648dcb73c7a25bcc067
Obed Okemsinachi <obedokemsinachi@gmail.com>
2023-03-08 23:25:19 +0100

    Changed .

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-function onclick(but) {
-    document.getElementsByTagName(h2).innerHTML= "My name is obed";
+var het = "My name is obed";
+
+var hat = "My name is Ronin";
+
+
+function but() {
+    if (display == 1) {
+        document.getElementById("he").innerHTML= het ;
+        display = 0;
+    } else {
+        document.getElementById("he").innerHTML= hat ;
+        display = 1;
+    }
+}
